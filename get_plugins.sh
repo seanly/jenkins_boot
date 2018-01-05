8bdc8df9b3b5a5feec82dce95183590665f9a3f8
Sean Ly <seanly@aliyun.com>
2018-01-05 14:46:31 +0800

    add get_plugins.sh script

---
+++ get_plugins.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+JENKINS_HOST=$1
+curl -sSL "$JENKINS_HOST/pluginManager/api/xml?depth=1&xpath=/*/*/shortName|/*/*/version&wrapper=plugins" | perl -pe 's/.*?<shortName>([\w-]+).*?<version>([^<]+)()(<\/\w+>)+/\1 \2\n/g'|sed 's/ /:/'
